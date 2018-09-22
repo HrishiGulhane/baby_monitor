cdde7e02cc9fe0d82731e35e2216d3a4117e7443
HrishiGulhane <gulhane.hrishikesh@gmail.com>
2018-09-22 12:26:18 +1000

    jquery install

--- window/js/led.js
+++ window/js/led.js
@@ -1,11 +1,31 @@
 var five = require("johnny-five");
+
+
 function setupBoard() {
     board = new five.Board({ repl: false });
   
-    board.on("ready", function() {
+    // board.on("ready", rgbtest());
+/*     board.on("ready", function() {
       led = new five.Led(13);
       led.blink(500);
       console.log("led initialised.")
     })
-    console.log("gitbuy");
+    console.log("gitbuy"); */
+  }
+
+  function rgbtest(){
+    var led = new five.Led.RGB({
+      pins: {
+        red: 9,
+        green: 10,
+        blue: 11
+      }
+    });
+  
+    // Turn it on and set the initial color
+    led.on();
+    led.color("#FF0000");
+  
+    led.blink(1000);
+  
   }
